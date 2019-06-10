943df3605bc4e4b236efa8659c719911e4d956e9
y03005b72 <229406183@qq.com>
2019-06-10 15:32:01 +0800

    测试提交git

---
+++ SpringbootApp/src/main/java/com/hipfun/SpringbootApp/SpringbootApp.java
@@ -0,0 +1,12 @@
+package com.hipfun.SpringbootApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApp {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootApp.class, args);
+    }
+}
